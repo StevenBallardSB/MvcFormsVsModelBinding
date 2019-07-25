4aca7ceb5a1ae3612edc67a7eda724b97346b9d7
steven ballard <stevenballardstudios@gmail.com>
2019-07-25 12:18:43 -0700

    Create Pro Team form with model binding

--- MvcFormsVsModelBinding/Controllers/ProTeamController.cs
+++ MvcFormsVsModelBinding/Controllers/ProTeamController.cs
@@ -2,15 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MvcFormsVsModelBinding.Models;
 
 namespace MvcFormsVsModelBinding.Controllers
 {
     public class ProTeamController : Controller
     {
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        //When data is posted to /ProTeam/Register
+        //this action will execute
+        // Add error messages on page to notify user
+
+        [HttpPost]
+        public IActionResult Register(IFormCollection data)
+        {
+            //Validate input
+            //Server-side and client-side
+
+            ProTeam pro = new ProTeam()
+            {
+                NameOfTeam = data["name-of-team"],
+                NameOfCoach = data["name-of-coach"],
+                Country = data["contry"],
+                Sport = data["sport"],
+                DateFounded = Convert.ToDateTime(data["dof"])
+            };
+
+            //Add to database
+            return View();
+        }
+
+        //Registration done with model binding
+        [HttpGet]
+        public IActionResult RegisterMB()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterMB(ProTeam p)
+        {
+            // This verifies all data annotation in the class are valid
+            if (ModelState.IsValid)
+            {
+                // Add to database
+                // Display success message
+            }
+
+            return View();
+        }
     }
+    
 }
---
+++ MvcFormsVsModelBinding/Models/ProTeam.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcFormsVsModelBinding.Models
+{
+    /// <summary>
+    /// Represents a Profesional Sports Team
+    /// </summary>
+    public class ProTeam
+    {
+        public string TeamId { get; set; }
+
+        /// <summary>
+        /// The name of the team
+        /// </summary>
+        [Required(ErrorMessage = "Team Name is required")]
+        [Display(Name = "Enter Team Name")]
+        public string NameOfTeam { get; set; }
+
+        /// <summary>
+        /// The name of the team's coach
+        /// </summary>
+        [Required(ErrorMessage = "Coach Name is required")]
+        [Display(Name = "Enter Coach Name")]
+        public string NameOfCoach { get; set; }
+
+        /// <summary>
+        /// The country that the team is from
+        /// </summary>
+        [Required(ErrorMessage = "Team's Country is required")]
+        [Display(Name = "Enter Team's Country")]
+        public string Country { get; set; }
+
+        /// <summary>
+        /// The sport that the team plays
+        /// </summary>
+        [Required(ErrorMessage ="Team's Sport is required")]
+        [Display(Name = "Enter Team's Sport")]
+        public string Sport { get; set; }
+
+        /// <summary>
+        /// When the team was founded
+        /// </summary>
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage ="Team's date of foundation is required")]
+        [Display(Name = "Enter Date of Team's Foundation")]
+        public DateTime DateFounded { get; set; }
+
+    }
+}
